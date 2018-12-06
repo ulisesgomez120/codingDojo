37add891f6af5396574c8244dad2f065916f66b5
ulisesgomez120 <ulisesgomez120@gmail.com>
2018-12-05 21:42:27 -0800

    added more flask_fun work

--- python_stack/flask_fundamentals/dojo_survey/server.py
+++ python_stack/flask_fundamentals/dojo_survey/server.py
@@ -10,6 +10,10 @@ def display_results():
     print(request.form)
     return render_template("result.html")
 
+@app.route('/danger')
+def nope():
+    print("a user tried to visit /danger.  we have redirected the user to /")
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True)
